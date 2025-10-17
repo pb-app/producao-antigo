2b1fdda4fa779bdfd4274307ca69e1b7f2fd00a6
pb-app <uegafs@gmail.com>
2025-10-17 01:04:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", event => {
   console.log("Service Worker instalado.");
   event.waitUntil(
-    caches.open("app-cache-v1").then(cache => {
+    caches.open("app-cache-v2").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
