385c17576e103523ddd3e7edf145f1918d651481
pb-app <uegafs@gmail.com>
2025-10-18 03:27:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // 1. Defina um nome de cache versionado. Mude este nome a cada atualização!
-const CACHE_NAME = 'app-cache-v6'; 
+const CACHE_NAME = 'app-cache-v7'; 
 
 // Lista de arquivos que serão cacheados
 const urlsToCache = [
